3a94ce0ccd350d0e4dd188c1162d0c5cdbbbb721
Janez <janez.cadez007@gmail.com>
2018-04-13 18:30:52 +0200

    components folder, restructuring, adding material-ui icons and normalize.css

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        React books app - step 2
+      </div>
+    );
+  }
+}
+
+export default App;
