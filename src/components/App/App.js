495c5bf5736187e68b65459b69e0653af1add391
Janez <janez.cadez007@gmail.com>
2018-04-16 18:27:53 +0200

    refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Button from 'material-ui/Button';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        React books app - step 2
-      </div>
+      <Button variant="raised" color="primary">
+        Hello World
+      </Button>
     );
   }
 }
